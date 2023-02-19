2525875890809fff8a002b55167118f906c78f79
Nikhitaveluri <71162889+Nikhitaveluri@users.noreply.github.com>
2023-02-19 14:58:54 +0530

    reading the data

--- code.py
+++ code.py
@@ -4,3 +4,25 @@ import numpy as np
 import geopandas as gpd
 import contextily as ctxdflfölfd
 import matplotlib.pyplot as plt
+
+#Reading the data:
+#url = 'https://proxy.hxlstandard.org/data/e2bb4b/download/jrc-covid-19-regions-hxl.csv'
+dtypes = {
+    'Date': object,
+    'iso3': object,
+    'CountryName': object,
+    'Region': object,
+    'lat': float,
+    'lon': float,
+    'CumulativePositive': float,
+    'CumulativeDeceased': float,
+    'CumulativeRecovered': float,
+    'CurrentlyPositive': float,
+    'Hospitalized': float,
+    'IntensiveCare': float,
+    'EUcountry': np.bool,
+    'EUCPMcountry': np.bool,
+    'NUTS': object,
+    }
+
+df = pd.read_csv("jrc-covid-19-regions-hxl.csv", skiprows=range(1, 2), dtype=dtypes)
